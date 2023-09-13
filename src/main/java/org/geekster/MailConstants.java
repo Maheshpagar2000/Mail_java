9839cd9c083a9f82abfb288b941c255e8f24f010
Mahesh Pagar <pagarmaheshofficial@gmail.com>
2023-09-13 09:54:14 +0530

    Mailing Application

---
+++ src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,7 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "maheshpagar2014@gmail.com";
+    public static final String APP_PASSWORD = "xxxxxxxxxxxxx";
+}
